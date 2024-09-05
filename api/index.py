6bb40c70a7225b85c41ed832413e3166db12b6ea
junaama <0naama0@gmail.com>
2024-09-05 13:59:10 -0400

    add websocket route

--- api/index.py
+++ api/index.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import List
 
 app = FastAPI()
+
+clients: List[WebSocket] = []
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in clients:
+                await client.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        clients.remove(websocket)
