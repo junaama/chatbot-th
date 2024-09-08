0732ca2ea6842a27c86ef2d64f89c1c475157d23
junaama <0naama0@gmail.com>
2024-09-08 17:47:37 -0400

    update auth hook

--- app/hooks/useAuth.tsx
+++ app/hooks/useAuth.tsx
@@ -1,21 +1,32 @@
 'use client'
 import { useCallback, useEffect, useState } from "react"
-import { getMe } from "../auth/auth"
+import { getMe, validateToken } from "../auth/auth"
 import { ReadUsersMeUsersMeGetResponse } from "../client"
 
-const isLoggedIn = () => {
-    return localStorage.getItem("access_token") !== null
-}
 
 const useAuth = () => {
     const [user, setUser] = useState<ReadUsersMeUsersMeGetResponse | null>(null)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const loggedIn = localStorage.getItem("access_token") !== null
 
     const fetchUser = useCallback(async () => {
-
-        if (isLoggedIn()) {
-            const userData = await getMe()
-            setUser(userData)
-
+        const token = localStorage.getItem("access_token")
+        if (token !== null) {
+            try {
+                const response = await validateToken()
+                if(response){
+                    const userData = await getMe()
+                    setUser(userData)
+                    setIsLoggedIn(true)
+                } else {
+                    localStorage.removeItem("access_token")
+                    setIsLoggedIn(false)
+                }
+            } catch (error) {
+                setIsLoggedIn(false)
+            }
+        } else {
+            setIsLoggedIn(false)
         }
     }, [])
 
@@ -24,8 +35,10 @@ const useAuth = () => {
     }, [fetchUser])
     
     return {
-        user
+        user,
+        fetchUser,
+        isLoggedIn,
+        loggedIn
     }
 }
-export {isLoggedIn}
 export default useAuth
